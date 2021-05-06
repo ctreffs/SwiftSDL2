2ee13c065f39df3ed50896e27381507ed36df2bb
Christian Treffs <ctreffs@gmail.com>
2021-05-06 10:46:07 +0200

    Try unsafeFlag workaround

---
+++ Sources/CSDL2Wrapped/CSDL2Wrapped.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christian Treffs on 06.05.21.
+//
+
+/// workaround for unsafeFlags from SDL <https://forums.swift.org/t/override-for-unsafeflags-in-swift-package-manager/45273/5>
+@_exported import CSDL2
