374273aec55078ba900cae2778a1e35925746c6c
pcbeard <pcbeard@mac.com>
2021-09-16 16:52:35 -0700

    Copy libraries into .build/{debug, release}

--- copylibs.ps1
+++ copylibs.ps1
@@ -1,4 +1,9 @@
 $bindir = (pkgconf --variable exec_prefix sdl2).Trim()
 $libdir = (pkgconf --variable libdir sdl2).Trim()
-Copy-Item ($bindir + '/SDL2.dll') .build/debug
-Copy-Item ($libdir + '/SDL2.lib') .build/debug
+foreach ($config in "debug", "release") {
+    $path = ".build/$config"
+    if (Test-Path -Path $path) {
+        Copy-Item ($bindir + '/SDL2.dll') $path
+        Copy-Item ($libdir + '/SDL2.lib') $path
+    }
+}
