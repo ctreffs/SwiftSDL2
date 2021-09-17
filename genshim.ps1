599924667c920cc9e2a215e8aa99ee85a86d4a6a
pcbeard <pcbeard@mac.com>
2021-09-17 16:11:23 -0700

    Add SDL_vulkan.h

--- genshim.ps1
+++ genshim.ps1
@@ -1,2 +1,3 @@
 $includedir = (pkgconf --variable includedir sdl2).Trim()
 Write-Output ('#include "' + $includedir + '/SDL2/SDL.h"') > Sources/CSDL2/windows_generated.h
+Write-Output ('#include "' + $includedir + '/SDL2/SDL_vulkan.h"') >> Sources/CSDL2/windows_generated.h
