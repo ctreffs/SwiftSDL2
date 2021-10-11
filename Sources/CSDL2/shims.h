b6a58e89a866e33aa3a14039e48901f076195895
Christian Treffs <ctreffs@gmail.com>
2021-10-11 16:20:59 +0200

    Add windows CI (#6)
    * Add windows CI
    
    * Update shims
    
    * Install SDL2
    
    * Install SDL2 x64 and pkg-config
    
    * x64 pkg-config
    
    * Update ci-windows.yml
    
    * Update ci-windows.yml
    
    * Update ci-windows.yml
    
    * Update macOS + Linux CI
    
    * Update windows CI
    
    * Update CI
    
    * Update CI
    
    * Update CI

--- Sources/CSDL2/shims.h
+++ Sources/CSDL2/shims.h
@@ -1,16 +1,28 @@
-#ifdef __APPLE__
+// <https://stackoverflow.com/a/5920028/6043526>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+   //define something for Windows (32-bit and 64-bit, this part is common)
+   #ifdef _WIN64
+      //define something for Windows (64-bit only)
+      error "Win64 unsupported"
+   #else
+      //define something for Windows (32-bit only)
+      error "Win32 unsupported"
+   #endif
+#elif __APPLE__
     #include <TargetConditionals.h>
     #if TARGET_IPHONE_SIMULATOR
-         #include "apple_iOS.h"
+         #include "apple_iOS.h" // iOS Simulator
     #elif TARGET_OS_IPHONE
-        #include "apple_iOS.h"
+        #include "apple_iOS.h" // iOS device
     #elif TARGET_OS_MAC
-        #include "apple_macOS.h"
+        #include "apple_macOS.h" //  macOS
     #else
        error "Unknown Apple platform"
     #endif
+#elif __linux__
+    #include "linux.h" //  linux
 #else
-	#include "other_platforms.h"
+   error "Unsupported platform"
 #endif
 
 // SDL_KeyCode was introduced with 2.0.12
