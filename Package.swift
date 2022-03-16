cefa979458cde6b3b6419bace97999727af2de93
Christian Treffs <ctreffs@gmail.com>
2022-03-16 13:46:06 +0100

    Add minimal SDL example

--- Package.swift
+++ Package.swift
@@ -6,6 +6,8 @@ let package = Package(
     products: [
         .library(name: "SDL2",
                  targets: ["SDL2"]),
+        .executable(name: "MinimalApp",
+                    targets: ["Minimal"])
     ],
     targets: [
         .systemLibrary(
@@ -18,6 +20,7 @@ let package = Package(
         .target(name: "SDL2", dependencies: ["CSDL2Wrapped"]),
         // workaround for unsafeFlags from SDL <https://forums.swift.org/t/override-for-unsafeflags-in-swift-package-manager/45273/5>
         .target(name: "CSDL2Wrapped", dependencies: ["CSDL2"]),
+        .target(name: "Minimal", dependencies: ["SDL2"], path: "Sources/Demos/Minimal"),
         .testTarget(name: "CSDL2Tests", dependencies: ["CSDL2"])
     ]
 )
---
+++ Sources/Demos/Minimal/main.swift
@@ -0,0 +1,37 @@
+import SDL2
+
+// Initialize SDL video systems
+guard SDL_Init(SDL_INIT_VIDEO) == 0 else {
+    fatalError("SDL could not initialize! SDL_Error: \(String(cString: SDL_GetError()))")
+}
+
+// Create a window at the center of the screen with 800x600 pixel resolution
+let window = SDL_CreateWindow(
+    "SDL2 Minimal Demo",
+    Int32(SDL_WINDOWPOS_CENTERED_MASK), Int32(SDL_WINDOWPOS_CENTERED_MASK),
+    800, 600,
+    SDL_WINDOW_SHOWN.rawValue)
+
+var quit = false
+var event = SDL_Event()
+
+// Run until app is quit
+while !quit {
+    // Poll for (input) events
+    while SDL_PollEvent(&event) > 0 {
+        // if the quit event is triggered ...
+        if event.type == SDL_QUIT.rawValue {
+            // ... quit the run loop
+            quit = true
+        }
+    }
+
+    // wait 100 ms
+    SDL_Delay(100)
+}
+
+// Destroy the window
+SDL_DestroyWindow(window)
+
+// Quit all SDL systems
+SDL_Quit()
