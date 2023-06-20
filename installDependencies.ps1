355c3b4d621336abe8303640df2e15e7d6f18f8a
Christian Treffs <ctreffs@gmail.com>
2023-06-20 15:16:38 +0200

    Update win ci

--- installDependencies.ps1
+++ installDependencies.ps1
@@ -1,24 +1,30 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 
+# install vcpkg
 git clone https://github.com/Microsoft/vcpkg.git
 .\vcpkg\bootstrap-vcpkg.bat
 .\vcpkg\vcpkg integrate powershell
 
+# install pkgconf
 .\vcpkg\vcpkg install pkgconf --triplet x64-windows --no-print-usage
 $env:Path += ';'+ $pwd.Path + '\vcpkg\installed\x64-windows\tools\pkgconf\'                
 $env:PKG_CONFIG_PATH = '' + $pwd.Path + '\vcpkg\installed\x64-windows\lib\pkgconfig'
 
+# install SDL
 .\vcpkg\vcpkg install sdl2[core,vulkan] --recurse --triplet x64-windows --no-print-usage
 
+# generate windows header
 $includedir = (pkgconf --variable includedir sdl2).Trim()
 Write-Output ('#include "' + $includedir + '/SDL2/SDL.h"') > Sources/CSDL2/windows_generated.h
 Write-Output ('#include "' + $includedir + '/SDL2/SDL_vulkan.h"') >> Sources/CSDL2/windows_generated.h
 
+# clean swift build
 Remove-Item -Path .\.build -Recurse
 
+# build
 swift build
 
-# copy libraries
+# copy SDL libraries
 $bindir = ((pkgconf --variable exec_prefix sdl2).Trim() + "/bin")
 $libdir = (pkgconf --variable libdir sdl2).Trim()
 foreach ($config in "debug", "release") {
@@ -29,4 +35,5 @@ foreach ($config in "debug", "release") {
     }
 }
 
+# test
 swift test
